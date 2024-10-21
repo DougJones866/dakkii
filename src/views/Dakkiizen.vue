208c912fe670d1762009ff7a8c2f4efb00148a9d
kibroman <you@example.comkibromanx@gmail.com>
2024-10-22 07:44:45 +0900

    updated

--- src/views/Dakkiizen.vue
+++ src/views/Dakkiizen.vue
@@ -1,18 +1,28 @@
 <template>
-<div>
-<img src="../assets/dakkiizenlogo.png" />
-
-</div>
-
-</template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-body{
-    background: white;
-}
-
-</style>
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/dakkiizenlogo.png" />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  /* The root container will take up the full viewport */
+  .container {
+      background-color: white; /* Override default background */
+      width: 100vw;  /* Full width of the viewport */
+      height: 100vh; /* Full height of the viewport */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  
+  .logo img {
+      width: 50%;
+      height: auto;
+  }
+  </style>
+  
